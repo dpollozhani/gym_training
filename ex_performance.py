a396ffe5f06e5bf7bb714deb9a8c2d8c3beddac9
Drilon Pollozhani <drilonpollozhani@Drilons-MBP.lan>
2021-05-06 19:57:44 +0200

    Update ex_performance.py

--- ex_performance.py
+++ ex_performance.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
 from datetime import datetime
 from typing import Tuple
+import base64
 from gym_sessions import GymSessionsDB
 
 def _get_log(db: GymSessionsDB) -> pd.DataFrame:
@@ -21,26 +23,54 @@ def _get_users(log: pd.DataFrame) -> list:
 def _get_exercises(log: pd.DataFrame) -> list:
     return list(set(log.exercise.values))
 
+def _get_table_download_link(log: pd.DataFrame):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = log.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}" download="gym_performance_log.csv">Export data</a>'
+    return href
+
 #Past performance
 def app(db, default_user):
     st.markdown('## Performance')
-    
-    log = _get_log(db)
+    full_log = _get_log(db)
 
-    available_users = _get_users(log)
-    user_multiselect = st.multiselect('Users', options=available_users, default=default_user)
+    available_users = _get_users(full_log)
+    available_exercises = _get_exercises(full_log)
+    min_datetime, max_datetime = _get_min_max_date(full_log)
     
-    available_exercises = _get_exercises(log)
-    exercise_multiselect = st.multiselect('Exercises', options=available_exercises, default=available_exercises)
-    
-    min_datetime, max_datetime = _get_min_max_date(log)
-    date_range = st.slider(
-        'Date range', 
-        min_value=min_datetime,
-        value=min_datetime,
-        max_value=max_datetime, 
-        format="YYYY/MM/DD"
-    )
-    log = log[(log['date'] >= date_range) & (log['user'].isin(user_multiselect)) & (log['exercise'].isin(exercise_multiselect))]
-    log['date'] = log['date'].dt.date
-    st.write(log)
+    with st.beta_container():
+        #Chart filters
+        selection_col1, selection_col2 = st.beta_columns((1,4))
+        user_select = selection_col1.selectbox('User', options=available_users)
+        exercise_multiselect = selection_col2.multiselect('Exercises', options=available_exercises, default=available_exercises)
+
+        date_range = st.slider(
+            'Date range', 
+            min_value=min_datetime,
+            value=min_datetime,
+            max_value=max_datetime, 
+            format="YYYY/MM/DD"
+        )
+
+        log = full_log[(full_log['date'] >= date_range) & (full_log['user'] == user_select) & (full_log['exercise'].isin(exercise_multiselect))]
+        log['date'] = log['date'].dt.date
+
+        #Chart
+        chart = alt.Chart(log).mark_circle().encode(
+            alt.X('date', scale=alt.Scale(clamp=False, zero=False)),
+            alt.Y('best_set_weight', scale=alt.Scale(zero=False, padding=1)),
+            color='exercise',
+            size='best_set_reps',
+            tooltip=['exercise', 'best_set_weight', 'best_set_reps', 'date'],
+            ).properties(height=500).interactive()
+        
+
+        st.altair_chart(chart, use_container_width=True)
+
+    with st.beta_expander('Show all data'):
+        st.write(full_log)
+        st.markdown(_get_table_download_link(full_log), unsafe_allow_html=True)
